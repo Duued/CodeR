444b5af3da59d8a731b609dc1aa46d8dc7d3f7c3
Duued <66633587+Duued@users.noreply.github.com>
2024-12-10 15:20:12 -0500

    Add code storage

--- main.lua
+++ main.lua
@@ -1,14 +1,40 @@
 ---@diagnostic disable: lowercase-global
 
+local codes = {}
+
 ---Generates a code with the specified number of digits
 ---@param digits number
 ---@return number
 function generateCode(digits)
-    local code  = ""
+    local code = ""
     for i = 1, digits do
         code = code .. math.random(0, 9)
     end
-    return tonumber(code) or 0
+    return code
+end
+
+---Adds a code
+---@param code number
+---@return nil
+function addCode(code)
+    table.insert(codes, code)
+end
+
+---Removes a code
+---@param code number|nil
+---@param index number|nil
+---@return nil
+function removeCode(code, index)
+    if code then
+        for i, v in ipairs(codes) do
+            if v == code then
+                table.remove(codes, i)
+                break
+            end
+        end
+    elseif index then
+        table.remove(codes, index)
+    end
 end
 
 ---Compares two codes
@@ -17,4 +43,16 @@ end
 ---@return boolean
 function compareCodes(correctCode, enteredCode)
     return correctCode == enteredCode
+end
+
+---Gets all stored codes
+---@return table
+function getCodes()
+    return codes
+end
+
+---Clears all stored codes
+---@return nil
+function clearCodes()
+    codes = {}
 end
