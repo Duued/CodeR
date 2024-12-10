3e8c02b78cf2a8c211835fd6259ff4c35328ef3c
Duued <66633587+Duued@users.noreply.github.com>
2024-12-10 12:05:46 -0500

    Add generateCode function

---
+++ main.lua
@@ -0,0 +1,12 @@
+
+---Generates a code with the specified number of digits
+---@param digits number
+function generateCode(digits)
+    local code  = ""
+    for i = 1, digits do
+        code = code .. math.random(0, 9)
+    end
+    return tonumber(code)
+end
+
+print(generateCode(4))
