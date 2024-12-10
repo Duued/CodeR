7b4f9e5070f9a4a95fa6937fefe29fcd3b64e1b5
Duued <66633587+Duued@users.noreply.github.com>
2024-12-10 14:35:24 -0500

    Add compareCodes

--- main.lua
+++ main.lua
@@ -1,10 +1,20 @@
+---@diagnostic disable: lowercase-global
 
 ---Generates a code with the specified number of digits
 ---@param digits number
+---@return number
 function generateCode(digits)
     local code  = ""
     for i = 1, digits do
         code = code .. math.random(0, 9)
     end
-    return tonumber(code)
+    return tonumber(code) or 0
+end
+
+---Compares two codes
+---@param correctCode number
+---@param enteredCode number
+---@return boolean
+function compareCodes(correctCode, enteredCode)
+    return correctCode == enteredCode
 end
